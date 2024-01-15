223f14a6760460d0a3855c1cf605b476ad92d9ec
JaskierBared <mateuszswiderski009@gmail.com>
2024-01-15 16:04:19 +0100

    Added island with image

---
+++ src/components/MainAbout/MainAbout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './MainAbout.css';
+
+export const MainAbout = () => {
+  return (
+    <div className="Place">
+        <img className="photo" src={process.env.PUBLIC_URL + '/images/cv.jpg'} alt="CV" />
+        <section className='text'>
+            <h1 className="name">Mateusz Świderski</h1>
+            <p>Obecnie:</p>
+            <p>Poszukuję pracy V</p>
+            <p>Gotowość do przeprowadzki</p>
+            <p>Praca na pełen etat</p>
+        </section>
+    </div>
+  );
+}
+
