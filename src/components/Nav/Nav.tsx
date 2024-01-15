3486348c21e8a5a910a40b172518dafe699a38fa
JaskierBared <mateuszswiderski009@gmail.com>
2024-01-15 16:03:21 +0100

    Added simply navbar

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+
+export const Nav = () => {
+  return (
+    <div className="Nav">
+      <button>Krótko o mnie</button>
+      <button>O mnie</button>
+      <button>Portfolio</button>
+      <button>Umiejętności</button>
+
+    </div>
+  );
+}
+
