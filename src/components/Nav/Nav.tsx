0a643b48d8ea2bc8e0e273d566d0fa3c4d721751
JaskierBared <mateuszswiderski009@gmail.com>
2024-01-17 12:41:26 +0100

    finished nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Nav.css';
+import { ChooseLanguage } from '../Language/ChooseLanguage';
 
 
 export const Nav = () => {
   return (
     <div className="Nav">
-      <button>Krótko o mnie</button>
-      <button>O mnie</button>
-      <button>Portfolio</button>
-      <button>Umiejętności</button>
+      <h1>CV</h1>
+      <h2>O mnie</h2>
+      <h2>Portfolio</h2>
+      <h2>Umiejętności</h2>
+    <button className='button'>Pobierz CV</button>
 
     </div>
   );
